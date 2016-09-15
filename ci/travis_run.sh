0b90c6502bdfe7fd753b0baaaeccd1a49a9b4adb
Nathanael Jones <nathanael.jones@gmail.com>
2016-09-15 12:09:02 -0600

    Make VALGRIND=True work across all C,C++,Rust unit and integration tests Make COVERAGE work as expected (was forced to True, making tests a debug build)
    Add valgrind suppressions

--- ci/travis_run.sh
+++ ci/travis_run.sh
@@ -28,20 +28,20 @@ export GIT_BRANCH_NAME=${GIT_BRANCH_NAME:-$(git symbolic-ref HEAD | sed -e 's,.*
 
 #Don't upload pull requests
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-	export UPLOAD_BUILD=${UPLOAD_BUILD:-true}
+	export UPLOAD_BUILD=${UPLOAD_BUILD:-True}
 else
-	export UPLOAD_BUILD=false
-	export UPLOAD_AS_LATEST=false
+	export UPLOAD_BUILD=False
+	export UPLOAD_AS_LATEST=False
 fi
 
 #
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     ${TRAVIS_BUILD_DIR}/ci/travis_run_osx.sh
 else
-    docker run --rm -v $HOME/.ccache:/home/conan/.ccache -v $HOME/.conan/data:/home/conan/.conan/data -v ${TRAVIS_BUILD_DIR}:/home/conan/imageflow -e "COVERALLS_TOKEN=${COVERALLS_TOKEN}" -e "JOB_NAME=${JOB_NAME}"  -e "UPLOAD_BUILD=${UPLOAD_BUILD}" -e "RUST_CHANNEL=${RUST_CHANNEL}" -e "COVERALLS=${COVERALLS}" -e "VALGRIND=${VALGRIND}" -e "GIT_BRANCH_NAME=${GIT_BRANCH_NAME}" -e "GIT_COMMIT=${GIT_COMMIT}" -e "JOB_BADGE=${JOB_BADGE}" -e "PACKAGE_PREFIX=${PACKAGE_PREFIX}"  -e "PACKAGE_SUFFIX=${PACKAGE_SUFFIX}" -e "UPLOAD_AS_LATEST=${UPLOAD_AS_LATEST}"  ${DOCKER_IMAGE} /bin/bash -c "./ci/travis_run_docker.sh"
+    docker run --rm -v $HOME/.ccache:/home/conan/.ccache -v $HOME/.conan/data:/home/conan/.conan/data -v ${TRAVIS_BUILD_DIR}:/home/conan/imageflow -e "COVERALLS_TOKEN=${COVERALLS_TOKEN}" -e "JOB_NAME=${JOB_NAME}"  -e "UPLOAD_BUILD=${UPLOAD_BUILD}" -e "RUST_CHANNEL=${RUST_CHANNEL}" -e "COVERAGE=${COVERAGE}" -e "COVERALLS=${COVERALLS}" -e "VALGRIND=${VALGRIND}" -e "GIT_BRANCH_NAME=${GIT_BRANCH_NAME}" -e "GIT_COMMIT=${GIT_COMMIT}" -e "JOB_BADGE=${JOB_BADGE}" -e "PACKAGE_PREFIX=${PACKAGE_PREFIX}"  -e "PACKAGE_SUFFIX=${PACKAGE_SUFFIX}" -e "UPLOAD_AS_LATEST=${UPLOAD_AS_LATEST}"  ${DOCKER_IMAGE} /bin/bash -c "./ci/travis_run_docker.sh"
 fi
 
-if [[ "$UPLOAD_BUILD" != 'true' ]]; then
+if [[ "$UPLOAD_BUILD" != 'True' ]]; then
 	echo -e "\nRemvoing all files scheduled for upload to s3\n\n"
 	rm -rf ./artifacts/upload
 	mkdir -p ./artifacts/upload
